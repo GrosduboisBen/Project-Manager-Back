4bad5a21cfff5a637f66f010aa95c93d4ce7898e
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2024-12-18 12:05:13 +0000

    Add created date to pricing

--- portfolio/db/models/pricing.py
+++ portfolio/db/models/pricing.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, DECIMAL, ForeignKey
+from sqlalchemy import Column, DECIMAL, ForeignKey, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 import uuid
 from portfolio.db.base_class import Base
+from datetime import datetime,timezone
 
 class Pricing(Base):
     __tablename__ = "pricing"
@@ -11,6 +12,7 @@ class Pricing(Base):
     price_per_day = Column(DECIMAL, nullable=False)
     tag_id = Column(UUID(as_uuid=True), ForeignKey("tags.id"), nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    creation_date = Column(TIMESTAMP, nullable=False, default=datetime.now(timezone.utc))
 
     tag = relationship("Tag", back_populates="pricing")
     user = relationship("User", back_populates="pricings")
