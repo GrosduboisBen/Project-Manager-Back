3635796d8dc2d96dcc84b5039be556a2d9d4ebae
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2024-12-18 11:16:40 +0000

    Milestones base crud

--- portfolio/db/models/milestones.py
+++ portfolio/db/models/milestones.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, String, ForeignKey, Text
+from sqlalchemy import Column, String, ForeignKey, Text, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 import uuid
 from portfolio.db.base_class import Base
+from datetime import datetime,timezone
 
 class Milestone(Base):
     __tablename__ = "milestones"
@@ -11,6 +12,7 @@ class Milestone(Base):
     project_id = Column(UUID(as_uuid=True), ForeignKey("projects.id"), nullable=False)
     title = Column(String, nullable=False)
     description = Column(Text)
+    creation_date = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(timezone.utc))
 
     project = relationship("Project", back_populates="milestones")
     missions = relationship("Mission", back_populates="milestone")
--- portfolio/main.py
+++ portfolio/main.py
@@ -5,6 +5,7 @@ from portfolio.core.config import settings
 from portfolio.routers.users.users import router as users_router
 from portfolio.routers.projects.projects import router as projects_router
 from portfolio.routers.missions.missions import router as missions_router
+from portfolio.routers.milestones.milestones import router as milestones_router
 # from portfolio.routers.roles.roles import router as roles_router
 # Import other routers as you add them
 
@@ -19,5 +20,6 @@ app = FastAPI(
 app.include_router(users_router, prefix=f"{settings.API_PREFIX}/users", tags=["Users"])
 app.include_router(projects_router, prefix=f"{settings.API_PREFIX}/projects", tags=["Projects"])
 app.include_router(missions_router, prefix=f"{settings.API_PREFIX}/missions", tags=["Missions"])
+app.include_router(milestones_router, prefix=f"{settings.API_PREFIX}/milestones", tags=["Milestones"])
 # app.include_router(roles_router, prefix=f"{settings.API_PREFIX}/roles", tags=["Roles"])
 # Add additional routes as necessary
