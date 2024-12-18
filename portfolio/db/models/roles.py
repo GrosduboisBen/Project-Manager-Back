811b4223cdc6f4a8ad3930ab4a2d7733b1aed248
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2024-12-18 11:20:14 +0000

    Add creation date to roles

--- portfolio/db/models/roles.py
+++ portfolio/db/models/roles.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 import uuid
 from portfolio.db.base_class import Base
+from datetime import datetime,timezone
 
 class Role(Base):
     __tablename__ = "roles"
@@ -10,6 +11,7 @@ class Role(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, unique=True, nullable=False)
     description = Column(String, nullable=False)
+    creation_date = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(timezone.utc))
 
     users = relationship("User", back_populates="role")
     permissions = relationship("Permission", back_populates="role")
