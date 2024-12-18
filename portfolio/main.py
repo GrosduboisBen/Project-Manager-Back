63ce3493633dc79803f4997efa52aaa65e80bf51
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2024-12-18 11:31:30 +0000

    Roles base crud

--- portfolio/main.py
+++ portfolio/main.py
@@ -6,7 +6,7 @@ from portfolio.routers.users.users import router as users_router
 from portfolio.routers.projects.projects import router as projects_router
 from portfolio.routers.missions.missions import router as missions_router
 from portfolio.routers.milestones.milestones import router as milestones_router
-# from portfolio.routers.roles.roles import router as roles_router
+from portfolio.routers.roles.roles import router as roles_router
 # Import other routers as you add them
 
 # Create the FastAPI app
@@ -21,5 +21,5 @@ app.include_router(users_router, prefix=f"{settings.API_PREFIX}/users", tags=["U
 app.include_router(projects_router, prefix=f"{settings.API_PREFIX}/projects", tags=["Projects"])
 app.include_router(missions_router, prefix=f"{settings.API_PREFIX}/missions", tags=["Missions"])
 app.include_router(milestones_router, prefix=f"{settings.API_PREFIX}/milestones", tags=["Milestones"])
-# app.include_router(roles_router, prefix=f"{settings.API_PREFIX}/roles", tags=["Roles"])
+app.include_router(roles_router, prefix=f"{settings.API_PREFIX}/roles", tags=["Roles"])
 # Add additional routes as necessary
